4227393801d3296dea5c9627b0d76290417739d8
nugrahaditya <adityanugrahadisini@gmail.com>
2021-10-10 20:33:32 +0700

    Remove unuse project

--- Moduit.Interview/Moduit.Interview.Core/Startup.cs
+++ Moduit.Interview/Moduit.Interview.Core/Startup.cs
@@ -26,6 +26,7 @@ namespace Moduit.Interview.Core
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo { Title = "Moduit.Interview", Version = "v1" }); });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +35,8 @@ namespace Moduit.Interview.Core
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Moduit.Interview v1"));
             }
 
             app.UseHttpsRedirection();
